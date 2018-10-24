b3bf3019e833ebe1b1c1c0a806cadba38a89124f
Arayik <araik.armenia@gmail.com>
2018-10-24 14:18:56 +0400

    Avelacrel em  erkrord classs

---
+++ src/sample2/Main.java
@@ -0,0 +1,9 @@
+package sample2;
+
+public class Main {
+    public static void main(String[] args){
+        while (true){
+            System.out.println("Es qez shatem sirum Aruk ");
+        }
+    }
+}
