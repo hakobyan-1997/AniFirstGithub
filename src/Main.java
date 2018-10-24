a939d3f78dee56ce16d30057b1480bc218f2c237
Arayik <araik.armenia@gmail.com>
2018-10-24 14:14:21 +0400

    Commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        while (true){
+            System.out.println("Es qez shatem sirum Anuliks ");
+        }
+
+    }
+}
